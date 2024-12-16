885ee9683c60b9f62f99bc4e1783a1ddcadd3b7e
NUNNA TARUN SRI VENKATA SAI <160224015+tarun1208@users.noreply.github.com>
2024-12-16 14:38:33 +0530

    Create lists.c

---
+++ lists.c
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>HELLO</title>
+  </head>
+  <body>
+    <h1>HTML Lists Demonstration</h1>
+      <h2>UNORDERED LIST</h2>
+      <ul>
+        <li>B.TECH</li>
+        <li>M.tech</li>
+        <li>Ph.D</li>
+      </ul>
+      <h2>ORDERED LIST</h2>
+      <ol>
+        <li>CSE</li>
+        <li>IT</li>
+        <li>ECE</li>
+      </ol>
+      <h2>NESTED LIST</h2>
+      <ul>
+        <li>II Year
+        <ul>
+          <li>Section-A</li>
+          <li>Section-B</li>
+          <li>Section-C</li>
+        </ul></li>
+        <li>III Year
+        <ul>
+          <li>Section-A</li>
+          <li>Section-B</li>
+        </ul></li>
+      </ul>
+      <h2>ORDERED LIST INSIDE UNORDERED LIST</h2>
+      <ul>
+        <li>Programming Language
+        <ol>
+          <li>Python</li>
+          <li>java</li>
+          <li>C++</li>
+        </ol></li>
+        <li>Database
+        <ol>
+          <li>MySQL</li>
+          <li>PostgreSQL</li>
+        </ol></li>
+      </ul>
+  </body>
+</html>
