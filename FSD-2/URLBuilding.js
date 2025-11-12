3367582e0c5ba32f78af029efd4ad0fb287a9e6b
NUNNA TARUN SRI VENKATA SAI <160224015+tarun1208@users.noreply.github.com>
2025-11-12 09:34:44 +0530

    Create URLBuilding.js

---
+++ FSD-2/URLBuilding.js
@@ -0,0 +1,28 @@
+const express = require("express")
+const app = express();
+
+ app.get("/",(req,res)=>{
+    res.send("Simple route")
+ });
+
+
+ app.get("/user/:name",(req,res)=>{
+    res.send(`user : ${req.params.name}`)
+ });
+
+
+app.get("/about",(req,res)=>{
+    res.send(`item : ${req.query.item} : price : ${req.query.price}`);
+})
+app.get("/link",(req,res)=>{
+    res.send(
+        `<a href="/user/tarun">User name</a><br>
+         <a href="/about?item=Black current Icecream&price=70">item </a>
+        `
+    );
+})
+
+app.listen(3050,()=>{
+    console.log("http://localhost:3050")
+})
+
